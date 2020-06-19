420a255d79f8a6c2edafee2061dd51042f62fe55
anthoninvanimpe <43902693+anthoninvanimpe@users.noreply.github.com>
2020-06-19 10:13:39 +0200

    Update routes.js

--- Router/routes.js
+++ Router/routes.js
@@ -2,9 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const User = require('../Models/user');
 
-app.post('/api/new-user', function(req,res){
+app.post('/api/exercise/new-user', function(req,res){
   var newUser = new User({username:req.body.username});
   
+  res.json({username:req.body.username, _id:req.body.id});
   
   
 });
+
+module.exports = routes;
