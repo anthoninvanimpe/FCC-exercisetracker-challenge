b6db5386b2fef8e5ba5c4d9bdc45400208bd0533
anthoninvanimpe <43902693+anthoninvanimpe@users.noreply.github.com>
2020-06-19 09:59:39 +0200

    Update user.js

--- Models/user.js
+++ Models/user.js
@@ -1,1 +1,11 @@
 
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username:{type: String, required: true},
+    creation_date: Date,
+    exercises:{type: mongoose.ObjectId, ref:"Exercise"}
+});
+
+module.exports = mongoose.model('User', userSchema);
